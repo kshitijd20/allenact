00c33fd996eb51b63628146904c9ec930b8b7fc7
kshitijd20 <kshitijdwivedi93@gmail.com>
2021-07-13 10:51:15 +0200

    added debug mode for single scene

--- projects/objectnav_baselines/experiments/ithor/objectnav_ithor_base.py
+++ projects/objectnav_baselines/experiments/ithor/objectnav_ithor_base.py
@@ -36,3 +36,32 @@ class ObjectNaviThorBaseConfig(ObjectNavThorBaseConfig, ABC):
             ]
         )
     )
+
+class ObjectNaviThorBaseConfigDebug(ObjectNavThorBaseConfig, ABC):
+    """The base config for all iTHOR ObjectNav experiments."""
+
+    THOR_COMMIT_ID = "bad5bc2b250615cb766ffb45d455c211329af17e"
+
+    DEFAULT_NUM_TRAIN_PROCESSES = 40 if torch.cuda.is_available() else 1
+
+    TRAIN_DATASET_DIR = os.path.join(os.getcwd(), "datasets/ithor-objectnav/train/debug")
+    VAL_DATASET_DIR = os.path.join(os.getcwd(), "datasets/ithor-objectnav/val/debug")
+
+    TARGET_TYPES = tuple(
+        sorted(
+            [
+                "AlarmClock",
+                "Apple",
+                "Book",
+                "Bowl",
+                "Box",
+                "Candle",
+                "GarbageCan",
+                "HousePlant",
+                "Laptop",
+                "SoapBottle",
+                "Television",
+                "Toaster",
+            ]
+        )
+    )
--- projects/objectnav_baselines/experiments/objectnav_mixin_ddppo.py
+++ projects/objectnav_baselines/experiments/objectnav_mixin_ddppo.py
@@ -46,3 +46,38 @@ class ObjectNavMixInPPOConfig(ObjectNavBaseConfig):
                 LambdaLR, {"lr_lambda": LinearDecay(steps=ppo_steps)}
             ),
         )
+
+
+class ObjectNavMixInPPOConfigDebug(ObjectNavBaseConfig):
+    def training_pipeline(self, **kwargs):
+        ppo_steps = int(300000)
+        lr = 3e-4
+        num_mini_batch = 1
+        update_repeats = 4
+        num_steps = 128
+        save_interval = 20000
+        log_interval = 10000 if torch.cuda.is_available() else 1
+        gamma = 0.99
+        use_gae = True
+        gae_lambda = 0.95
+        max_grad_norm = 0.5
+        return TrainingPipeline(
+            save_interval=save_interval,
+            metric_accumulate_interval=log_interval,
+            optimizer_builder=Builder(optim.Adam, dict(lr=lr)),
+            num_mini_batch=num_mini_batch,
+            update_repeats=update_repeats,
+            max_grad_norm=max_grad_norm,
+            num_steps=num_steps,
+            named_losses={"ppo_loss": PPO(**PPOConfig)},
+            gamma=gamma,
+            use_gae=use_gae,
+            gae_lambda=gae_lambda,
+            advance_scene_rollout_period=self.ADVANCE_SCENE_ROLLOUT_PERIOD,
+            pipeline_stages=[
+                PipelineStage(loss_names=["ppo_loss"], max_stage_steps=ppo_steps)
+            ],
+            lr_scheduler_builder=Builder(
+                LambdaLR, {"lr_lambda": LinearDecay(steps=ppo_steps)}
+            ),
+        )
